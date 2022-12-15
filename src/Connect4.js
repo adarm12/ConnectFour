fe3b191faa476344c1984b4d4460fae524f6d73c
adarm12 <adarm1234@gmail.com>
2022-12-15 18:27:42 +0200

    1

---
+++ src/Connect4.js
@@ -0,0 +1,9 @@
+function Connect4() {
+    return (
+        <div style={{textAlign: "center", fontSize: "50px"}}>
+            Connect4
+        </div>
+    )
+}
+
+export default Connect4;
--- src/DropButton.js
+++ src/DropButton.js
@@ -3,7 +3,6 @@ function DropButton(props){
         <button>
             drop
         </button>
-
     )
 }
 
