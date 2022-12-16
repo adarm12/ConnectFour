bb0ece4afd94a5c5355dedbd08dad4ad330af56b
adarm12 <adarm1234@gmail.com>
2022-12-16 16:17:32 +0200

    1

--- src/App.js
+++ src/App.js
@@ -7,25 +7,25 @@ import DropButton from "./DropButton";
 class App extends React.Component {
 
     state = {
-        board: [[0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0],
-            [["button"],["button"],["button"],["button"],["button"],["button"],["button"]]],
+        board: [[0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [["button"], ["button"], ["button"], ["button"], ["button"], ["button"], ["button"]]],
         currentPlayer: 1
     };
 
     drop = (index) => {
         let newBoard = this.state.board;
         let i;
-        for (i=this.state.board.length-1; i>=0; i--) {
+        for (i = this.state.board.length - 1; i >= 0; i--) {
             if (newBoard[i][index] === 0) {
                 break;
             }
         }
-        if (i>=0) {
+        if (i >= 0) {
             newBoard[i][index] = this.state.currentPlayer;
             this.setState({
                 board: newBoard,
@@ -34,23 +34,23 @@ class App extends React.Component {
         }
     }
 
-    render () {
+    render() {
         return (
             <div className="App">
                 <Connect4> </Connect4>
                 <table style={{alignItems: "center"}}>
-                    {this.state.board.map((row,index) => {
+                    {this.state.board.map((row, index) => {
                         return (
                             <tr>
                                 {row.map((cell, cellIndex) => {
                                     return (
                                         <td>
-                                            {cell===0?<Circle color = "white"/>:
-                                                (cell===1? <Circle color = "red"/>:
-                                                    (cell===2? <Circle color = "orange"/>:
-                                                        <DropButton
+                                            {cell === 0 ? <Circle color="white"/> :
+                                                (cell === 1 ? <Circle color="red"/> :
+                                                    (cell === 2 ? <Circle color="orange"/> :
+                                                <DropButton
                                                             index={cellIndex}
-                                                            dropCircle = {this.drop}/>))}
+                                                            dropCircle={this.drop}/>))}
                                         </td>
                                     )
                                 })}
